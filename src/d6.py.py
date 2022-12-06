8de0a35068feeece541f99944e4c9c8933089cd7
Jared-Johnson <jaj53223@bethel.edu>
2022-12-06 06:22:01 -0600

    Add doc string

--- src/d6.py.py
+++ src/d6.py.py
@@ -3,10 +3,14 @@ with open("../data/d6.txt", "r") as f:
     code = f.read()
 
 
-def search(length):
+def find_marker(length: str):
+    """
+    Finds index in a string where the previous "length" characters
+    are all unique.
+    """
     for i in range(length, len(code)):
         if len(set(code[i : i + length])) == length:
             return i + length
 
 
-print(f"Part 1: {search(4)}\nPart 2: {search(14)}")
+print(f"Part 1: {find_marker(4)}\nPart 2: {find_marker(14)}")
