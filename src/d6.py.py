e564e2e35b26b622d316445fa11968d54308124a
Jared-Johnson <jaj53223@bethel.edu>
2022-12-06 06:53:42 -0600

    Day 6

--- src/d6.py.py
+++ src/d6.py.py
@@ -3,14 +3,14 @@ with open("../data/d6.txt", "r") as f:
     code = f.read()
 
 
-def find_marker(length: str):
+def find_marker(string: str, length: int):
     """
     Finds index in a string where the previous "length" characters
     are all unique.
     """
-    for i in range(length, len(code)):
-        if len(set(code[i : i + length])) == length:
+    for i in range(length, len(string)):
+        if len(set(string[i : i + length])) == length:
             return i + length
 
 
-print(f"Part 1: {find_marker(4)}\nPart 2: {find_marker(14)}")
+print(f"Part 1: {find_marker(code, 4)}\nPart 2: {find_marker(code, 14)}")
