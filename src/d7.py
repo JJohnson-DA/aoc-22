4e1a9978f21633277494dda6c8b0e5f7cdcd0cd6
Jared Johnson <89032304+JJohnson-DA@users.noreply.github.com>
2022-12-07 09:53:43 -0600

    Day 7 Solved

--- src/d7.py
+++ src/d7.py
@@ -1,12 +1,11 @@
 #%%
-# first guess 1038055
-
 with open("../data/d7.txt", "r") as f:
     output = [x.rstrip() for x in f.readlines()]
 
 structure = {}
 current_dir = []
 
+# ---- Part 1 ----
 for line in output:
     size = 0
     # Remove a level from current directory
@@ -17,16 +16,26 @@ for line in output:
         current_dir = ["/"]
         if "/" not in structure:
             structure["/"] = 0
-    # Append to current directory, and add to structure if needed
+    # Append to current dir, and add to structure if needed
     elif line.startswith("$ cd"):
-        dir = line.split(" ")[-1]
+        dir = line.split()[-1]
         current_dir.append(dir)
-        if dir not in structure:
-            structure[dir] = 0
+        full_path = "/".join(current_dir)
+        if full_path not in structure:
+            structure[full_path] = 0
     # add file size to all directories in the current dir
     elif line[0].isdigit():
-        size = int(line.split(" ")[0])
-        for dir in current_dir:
-            structure[dir] += size
+        size = int(line.split()[0])
+        for i in range(1, len(current_dir) + 1):
+            partial_path = "/".join(current_dir[:i])
+            structure[partial_path] += size
+p1_total = sum(x for x in structure.values() if x <= 100000)
 
-total = sum(x for x in structure.values() if x <= 100000)
+# ---- Part 2 ----
+need_to_delete = structure["/"] - 40000000
+
+min_dir_size = min([size for size in structure.values() if size >= need_to_delete])
+
+
+print(f"Part 1: {p1_total}")
+print(f"Part 2: {min([size for size in structure.values() if size >= need_to_delete])}")
